93845c93046e4a131e29de6cba126a84cab5d58e
Masibo <victor.wafula@student.moringaschool.com>
2023-07-12 14:37:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+var customerName = "bob"
+function fixTheScope() {
+    return customerName;
+}
+console.log(fixTheScope());
+ 
+function upperCaseCustomerName() {
+   let customerName = "bob";
+   customerName = customerName.toUpperCase();
+   return customerName;
+}
+console.log(upperCaseCustomerName());
+
+bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "John Doe";
+  leastFavoriteCustomer = "Masibo"; // This line will cause an error
+  return leastFavoriteCustomer;
+}
+
+console.log (changeLeastFavoriteCustomer());
+
+
